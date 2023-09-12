6d2858a50594dddbbb678b9697080a8949a51c6a
Simon <64006503+simon-hjp@users.noreply.github.com>
2023-09-12 14:25:06 +0200

    Update text_classification.py
    Data import

--- text_classification.py
+++ text_classification.py
@@ -1,3 +1,13 @@
-import pandas
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-df = 
+# import data
+df = pd.read_table("", names=["Datapoint"])
+df["Datapoint"] = df["Datapoint"].str.lower()
+df[["dialog_act", "utterance_content"]] = df["Datapoint"].str.split(pat=" ", n=1, expand=True)
+df.drop("Datapoint", axis=1, inplace=True)
+# sanity check TODO remove when assignment is done
+print(df.head())
+
+# create train- and test set
+train_df, test_df = train_test_split(df, test_size=0.15)
