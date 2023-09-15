361eb35385cfcf3b0835c8baa6dc225b433d1932
Simon <64006503+simon-hjp@users.noreply.github.com>
2023-09-15 10:09:41 +0200

    Update text_classification.py
    Fixed rule baseline

--- text_classification.py
+++ text_classification.py
@@ -1,8 +1,47 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+###
+##### Data importing and preprocessing
+###
+
+def import_data(data_dir: str):
+    """Read data and return it as a dataframe.
+
+    Parameters:
+    - data_dir: {string} the directory where the .dat file is located.
+    """
+    # read data into DataFrame
+    df = pd.read_table(data_dir, names=["Datapoint"])
+    # to lowercase
+    df["Datapoint"] = df["Datapoint"].str.lower()
+    # split rows into labels and utterances
+    df[["dialog_act", "utterance_content"]] = df["Datapoint"].str.split(
+        pat=" ", n=1, expand=True
+    )
+    df.drop("Datapoint", axis=1, inplace=True)
+    # create train- and test set
+    df_train, df_test = train_test_split(df, test_size=0.15)
+    return df_train, df_test
+
+def transform_data(df_train: pd.DataFrame, df_test: pd.DataFrame):
+    """Transform utterance dataframe into machine-readable bag-of-words representation."""
+    labelencoder = LabelEncoder()
+    labelencoder.fit(df_train["dialog_act"])
+    labelencoder.transform(df_train["dialog_act"])
+    labelencoder.transform(df_test["dialog_act"])
+    # labelencoder
+    # bagofwords procedure
+
+###
+##### Baseline classifiers
+###
 
 class RuleBaselineClassifier():
+    """Baseline model that classifies utterances based on keywords.
+    """
     def utterance_contains_word(self, utterance, words):
         """Helper function to determine whether an utterance contains any word in a given list"""
         for word in words:
@@ -10,45 +49,41 @@ class RuleBaselineClassifier():
                 return True
         return False
 
-    def predict_act(self, utterances) -> list:
+    def predict_act(self, utterance) -> str:
         """Predict the dialog act of utterances using a set of rules."""
-        out = []
-        for utterance in utterances:
-            if self.utterance_contains_word(utterance, ["food", "restaurant", "town", "east", "west", "south", "north", "part"]):
-            # if "any" in utterance or "food" in utterance or "restaurant" in utterance or "town" in utterance or "part" in utterance or "east" in utterance or "west" in utterance:
-                out.append("inform")
-            elif "it" in utterance and "is" in utterance:
-                out.append("confirm")
-            elif self.utterance_contains_word(utterance, ["yes", "right"]):
-                out.append("affirm")
-            elif self.utterance_contains_word(utterance, ["number", "phone", "address", "post"]):
-                out.append("request")
-            elif "thank" in utterance and "you" in utterance:
-                out.append("thankyou")
-            elif self.utterance_contains_word(utterance, ["noise", "sil", "unintelligible"]):
-                out.append("null")
-            # elif "good" in utterance or "bye" in utterance:
-            elif self.utterance_contains_word(utterance, ["good", "bye"]):
-                out.append("bye")
-            elif ("how" in utterance and "about" in utterance) or "else" in utterance:
-                out.append("reqalts")
-            elif "no" in utterance:
-                out.append("negate")
-            elif self.utterance_contains_word(utterance, ["hi", "hello"]):
-                out.append("hello")
-            elif ("repeat" in utterance and "that" in utterance) or "repeat" in utterance:
-                out.append("repeat")
-            elif "okay" in utterance:
-                out.append("ack")
-            elif ("start" in utterance and "over" in utterance) or "start" in utterance:
-                out.append("restart")
-            elif self.utterance_contains_word(utterance, ["wrong", "dont"]):
-                out.append("deny")
-            elif "more" in utterance:
-                out.append("reqmore")
-            else:
-                out.append("inform")  # Can replace this with 'error' later on if necessary for evaluation.
-        return out
+        if self.utterance_contains_word(utterance, ["food", "restaurant", "town", "east", "west", "south", "north", "part"]):
+            return "inform"
+        elif "it" in utterance and "is" in utterance:
+            return "confirm"
+        elif self.utterance_contains_word(utterance, ["yes", "right"]):
+            return "affirm"
+        elif self.utterance_contains_word(utterance, ["number", "phone", "address", "post"]):
+            return "request"
+        elif "thank" in utterance and "you" in utterance:
+            return "thankyou"
+        elif self.utterance_contains_word(utterance, ["noise", "sil", "unintelligible"]):
+            return "null"
+        elif self.utterance_contains_word(utterance, ["good", "bye"]):
+            return "bye"
+        elif ("how" in utterance and "about" in utterance) or "else" in utterance:
+            return "reqalts"
+        elif "no" in utterance:
+            return "negate"
+        elif self.utterance_contains_word(utterance, ["hi", "hello"]):
+            return "hello"
+        elif ("repeat" in utterance and "that" in utterance) or "repeat" in utterance:
+            return "repeat"
+        elif "okay" in utterance:
+            return "ack"
+        elif ("start" in utterance and "over" in utterance) or "start" in utterance:
+            return "restart"
+        elif self.utterance_contains_word(utterance, ["wrong", "dont"]):
+            return "deny"
+        elif "more" in utterance:
+            return "reqmore"
+        else:
+            return "inform"  # Can replace this with 'error' later on if necessary for evaluation.
+
 
 class MajorityBaselineClassifier:
     """Baseline model for text classification that classifies every input as the most common class
@@ -76,6 +111,11 @@ class MajorityBaselineClassifier:
         """
         return self.prediction
 
+###
+##### Machine learning classifiers
+###
+
+
 
 def run():
     """Test each model, report performance, and then initiate the command-line for the user.
@@ -84,15 +124,16 @@ def run():
     # majority baseline
     majority_model = MajorityBaselineClassifier()
     majority_model.fit_training_data(df_train)
-    # evaluate_model(majority_model, df_test)
+    evaluate_model(majority_model, df_test)
 
     # rule-based baseline
-    baseline_model = RuleBaselineClassifier()
-    evaluate_model(baseline_model, df_test)
+    rule_model = RuleBaselineClassifier()
+    evaluate_model(rule_model, df_test)
 
     # decision tree
     
 
+    # other ML algorithm
 
     user_choice = input(
         "Please specify which model you want to test:\n\
@@ -101,10 +142,11 @@ def run():
             C: machine-learning classifier 1\n\
             D: machine-learning classifier 2\n"
     ).upper()
+    model = None
     if user_choice == "A":
         model = majority_model
     elif user_choice == "B":
-        model = RuleBaseline()
+        model = rule_model
     elif user_choice == "C":
         print("Please choose another model, since this one has not been implemented\n")
         run()
@@ -117,29 +159,6 @@ def run():
     user_testing(model)
     return
 
-
-def import_data(data_dir: str):
-    """Read data and return it as a dataframe.
-
-    Parameters:
-    - data_dir: {string} the directory where the .dat file is located.
-    """
-    # read data into DataFrame
-    df = pd.read_table(data_dir, names=["Datapoint"])
-    # to lowercase
-    df["Datapoint"] = df["Datapoint"].str.lower()
-    # split rows into labels and utterances
-    df[["dialog_act", "utterance_content"]] = df["Datapoint"].str.split(
-        pat=" ", n=1, expand=True
-    )
-    df.drop("Datapoint", axis=1, inplace=True)
-    # create train- and test set
-    df_train, df_test = train_test_split(df, test_size=0.15)
-    return df_train, df_test
-
-def create_bags(df: pd.DataFrame):
-
-
 def user_testing(model):
     """Predict an utterance given by the user with a trained model.
 
@@ -147,8 +166,7 @@ def user_testing(model):
     - model: {string} specify which model should be used.
 
     """
-    continue_testing = True
-    while continue_testing:
+    while True:
         user_utterance = input(
             "Please provide the sentence the model has to classify. \nTo exit the program, enter '1'.\n"
         )
