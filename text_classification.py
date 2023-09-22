6a94a571ece8e8c52dba491962a14f3b7d6e747a
Ingólfur Ari Jóhannsson <ingoar97@gmail.com>
2023-09-22 17:19:46 +0200

    updated diagram

--- text_classification.py
+++ text_classification.py
@@ -528,6 +528,7 @@ def command_line_testing(models_dict: dict):
 
 def run(data_dir="Data\\dialog_acts.dat"):
     """Train models and report their performance, then initiate the command line testing."""
+
     trained_models = create_models(data_dir=data_dir)
     command_line_testing(trained_models)
 
