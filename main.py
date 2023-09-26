a445da152ad4f492c67798fd501b73ad3ea30fa2
Simon <64006503+simon-hjp@users.noreply.github.com>
2023-09-26 12:38:14 +0200

    Added new required properties to restaurants database

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ import fsm_statemanager as fsm
 import utterance_extraction_recommendation as uer
 import levenshtein_spellchecking as ls
 import pandas as pd
+import numpy as np
 
-# global variables here
+# global variables for configurability
 spell_checking = True
+ask_spell_checking_confirmation = True
+output_all_caps = False
+add_output_delay = False
 
 # data imports
 dialog_training_df, dialog_testing_df = text_classification.import_data(data_dir="Data/dialog_acts.dat", drop_duplicates=True)
 restaurants_database = pd.read_csv("Data/restaurant_info.csv")
+
+# add additional properties to restaurants_db
+food_quality_vals = ['good food', 'mediocre food', 'bad food']
+crowdedness_vals = ['busy', 'quiet']
+length_stay_vals = ['long stay', 'medium stay', 'short stay']
+restaurants_database['food_quality'] = np.random.choice(food_quality_vals, restaurants_database.shape[0])
+restaurants_database['crowdedness'] = np.random.choice(crowdedness_vals, restaurants_database.shape[0])
+restaurants_database['length_stay'] = np.random.choice(length_stay_vals, restaurants_database.shape[0])
 
 # initialize and train dialog act classifier
 classifier = classifiers.DialogActsClassifier()
